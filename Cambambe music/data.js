a3807a5673be368933294121ac3358d73d53c695
Twoffaustino <136985931+Twoffaustino@users.noreply.github.com>
2023-10-29 01:50:52 -1000

    c

--- Cambambe music/data.js
+++ Cambambe music/data.js
@@ -24,5 +24,29 @@ let songs = [
      artist: "Baby do Mundinho",
      cover: "img/baby do mundinho.jpg"
    }, 
+   {
+    name: "SON STREET",
+    path: "music/Son Street - Inveja (Rap Drill Melodic).mp3",
+    artist: "Inveja",
+    cover: "img/son.jpg"
+  },
+  {
+    name: "BC MUSIC FT PAULO LEMOS",
+    path: "music/BC MUSIK - Work feat Paulo Lemos.mp3",
+    artist: "WORK",
+    cover: "img/bc ft paulo.jpg"
+  }, 
+  {
+    name: "BUNGA",
+    path: "music/bunga_superei_oficial_video_mp3_84946.mp3",
+    artist: "SUPEREI",
+    cover: "img/bunga.jpg"
+  }, 
+  {
+    name: "JELEX MUSIC",
+    path: "music/Tentaçaão  (M.Gang) P_k-1_022052.mp3",
+    artist: "TENTAÇÃO",
+    cover: "img/jelex.jpg"
+  }, 
  
 ]
